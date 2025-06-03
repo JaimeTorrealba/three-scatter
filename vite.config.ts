6f9a37b9330aee39d7fbec6f6a3e6bbc1478fc62
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2025-06-03 19:44:03 +0100

    🚀 Fix bundle three.js by error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import { visualizer } from "rollup-plugin-visualizer";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-    build: {
-        lib: {
-          entry: resolve(__dirname, 'src/index.ts'),
-          name: 'three-scatter',
-          // the proper extensions will be added
-          fileName: 'three-scatter',
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        })
+      ],
+     external: ['three'],
+      output: {
+        globals: {
+          three: 'THREE',
         },
-    }
+      },
+    },
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'three-scatter',
+      fileName: 'three-scatter',
+    },
+  },
 })
